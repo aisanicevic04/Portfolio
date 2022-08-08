f1cb1a0cfd5c3f70229e63b1e9421c51723fd8e4
Aisa Nicevic <aisanicevic04@gmail.com>
2022-08-08 12:42:27 +0200

    commit

--- script.js
+++ script.js
@@ -48,14 +48,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", "Python"],
+    strings: ["new projects", "new knowledge"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", "Python"],
+    strings: [],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
