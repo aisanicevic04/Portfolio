33b5d660402eb8e683ffae4686be8742352938ae
Aisa Nicevic <aisanicevic04@gmail.com>
2022-08-08 12:44:09 +0200

    commit

--- script.js
+++ script.js
@@ -54,13 +54,6 @@ $(document).ready(function () {
     loop: true,
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: [],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
   // owl carousel script
   $(".carousel").owlCarousel({
     margin: 20,
